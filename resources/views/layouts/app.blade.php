5aecbbd88ea0a3e68df9ba2120b17ea3c8e59df9
Noppanut Ploywong <noppanut15@hotmail.com>
2016-11-16 14:48:13 +0700

    Fix bug on author fb-og.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta property="og:type" content="article" />
         <meta name="author" content="@yield('fb-og-author', 'SheetPub')" />
-        <meta property="og:description" content="@yield('fb-og-description', 'ร่วมเรียนรู้และแบ่งปันเนื้อหาของคุณผ่าน "ชีทผับ" แหล่งรวมชีทออนไลน์ที่ทุกคนสามารถเข้าถึงได้ทุกที่ทุกเวลา')" />
+        <meta property="og:description" content="@yield('fb-og-description', 'ร่วมเรียนรู้และแบ่งปันเนื้อหาของคุณผ่านชีทผับ แหล่งรวมชีทออนไลน์ที่ทุกคนสามารถเข้าถึงได้ทุกที่ทุกเวลา')" />
         <meta property="article:author" content="@yield('fb-og-author', 'SheetPub')" />
         <meta property="article:publisher" content="@yield('fb-og-author', 'SheetPub')" />
         <meta property="og:image" content="{{{ asset('images/fb-og.png') }}}" />
