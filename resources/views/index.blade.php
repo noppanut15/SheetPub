4385b35a4b7a914f87586122738e512a57eb0b05
Noppanut Ploywong <noppanut15@hotmail.com>
2016-11-16 17:28:54 +0700

    Change fb-og image & footer.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -256,7 +256,7 @@
 <footer>
 	<div class="write_by">
 		<span>
-		Made with <i class="fa fa-heart" aria-hidden="true"></i>&nbsp;&nbsp; — &nbsp;&nbsp;SheetPub Team
+		Made with <i class="fa fa-heart" style="color:#FF8C74;" aria-hidden="true"></i>&nbsp;&nbsp; — &nbsp;&nbsp;SheetPub Team
 		</span>
 	</div>
 </footer>
